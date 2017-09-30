2e59e838e316c049202d7d8f1af969fa6fda74de
Zab <zabsalahid@gmail.com>
2017-09-30 16:34:11 +0800

    -some modification

--- test.js
+++ test.js
@@ -1,22 +1,19 @@
+'use strict'
+
 const geolocation = require('./lib/geolocation-360')()
 
 let params = {
-	cid: 31719,
-	lac: 11020,
-	mcc: 515,
-	mnc: 03
+	cid: '31719',
+	lac: '11020',
+	mcc: '515',
+	mnc: '03',
+	apiKey: 'AIzaSyDCv4Q5PZgs6G4RIirGqKXZlXpelDDA5C4'
 }
 
-console.log(geolocation)
-
-// let a = () => {
-	geolocation.requestGoogle(params, (err, result) => {
-		if (result) {
-			console.log(result)
-		} else {
-			console.log(err)
-		}
-	})
-// }
-
-// setInterval(a, 5000)
+geolocation.requestGoogle(params, (err, result) => {
+	if (result) {
+		console.log(result)
+	} else {
+		console.log('nag error: ' + err.message)
+	}
+})
